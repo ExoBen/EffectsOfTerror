1f4cc281c8f207d327de1c294309eae0b3e3c15d
ExogenesisBen <benatkilmory@gmail.com>
2015-02-21 22:23:07 +0000

    searchGTD working successfully with Haskell, and .gitignore updated to reflect compiled Haskell code not needed on server

--- EffectsOfTerror/EffectsOfTerror/searchGTD.py
+++ EffectsOfTerror/EffectsOfTerror/searchGTD.py
@@ -4,11 +4,11 @@ def search(searchParam, maxNum):
     #Use grep to search file and gather results
     c1 = ['grep', "-F", "-m", maxNum, searchParam, "../GTD.csv"]
     p1 = subprocess.Popen(c1, stdout=subprocess.PIPE)
-
     #Pass results of grep to the Haskell script for parsing
     c2 = ['../resultsParser']
     p2 = subprocess.Popen(c2, stdin=p1.stdout,stdout=subprocess.PIPE)
-
     #Return the result of the Haskell script
     result = p2.stdout.read()
-    return result
+    data = eval(result)
+    return data
+
