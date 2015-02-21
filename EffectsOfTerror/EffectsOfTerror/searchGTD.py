e66f0637c20067be76bde7feaf9daaade3e7ab8e
Isaac Jordan <sheepses6@gmail.com>
2015-02-21 17:29:10 +0000

    Added search to all pages.

--- EffectsOfTerror/EffectsOfTerror/searchGTD.py
+++ EffectsOfTerror/EffectsOfTerror/searchGTD.py
@@ -2,7 +2,7 @@ import subprocess
 
 def search(searchParam):
     #Use grep to search file and gather results
-    c1 = ['grep', "-F", searchParam, "../README.md"]
+    c1 = ['grep', "-F", searchParam, "../GTD.csv"]
     p1 = subprocess.Popen(c1, stdout=subprocess.PIPE)
 
     #Pass results of grep to the Haskell script for parsing
